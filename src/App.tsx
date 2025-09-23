7deb16e08c25dc28f3da13e48d652070a2921cc7
Pedro Giovanni <pedrogfp2016@gmail.com>
2025-09-23 14:14:45 -0300

    23/09_14:14

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Heading } from './components/Heading';
 
 export function App() {
-  console.log("Oi");
   return (
     <div>
-      <h1>Olá mundo (do App)</h1>
+      <Heading />
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt exercitationem reiciendis in veniam nisi tempora magni, dolorum iste! Tenetur corrupti possimus maiores quisquam distinctio praesentium voluptatum esse aperiam veniam eum?</p>
     </div>
   );
